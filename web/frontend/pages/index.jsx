79161ef6edbce07d7efd44f404242d686a22eb3a
shviam79315 <shivam.79315@gmail.com>
2025-09-22 12:39:42 +0530

    first commit

---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,23 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Text,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+import { useTranslation, Trans } from "react-i18next";
+
+import { trophyImage } from "../assets";
+
+import { ProductsCard } from "../components";
+
+export default function HomePage() {
+  const { t } = useTranslation();
+  return (
+    <h1>This is the home page</h1>
+  );
+}
